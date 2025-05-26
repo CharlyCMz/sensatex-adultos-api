af7658549d02104ad47daea141b1416e86182e66
Charly Correa <chuck.scrumdev@gmail.com>
2025-05-26 12:55:37 -0500

    Basic project structure with lint and editor config addons

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
+import { SalesModule } from './sales/sales.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, UsersModule, SalesModule],
   controllers: [AppController],
   providers: [AppService],
 })
